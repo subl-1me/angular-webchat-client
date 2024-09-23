e0eb1928cf0f648d92250c13bd9f89207f9d2ffd
Julio Ugalde <julio.ugalde404@gmail.com>
2024-09-22 19:01:17 -0600

    Content type phrase was writted well

--- src/app/request-list/data-access/chat.service.ts
+++ src/app/request-list/data-access/chat.service.ts
@@ -6,30 +6,39 @@ import { Observable } from 'rxjs';
 import { Request } from 'src/app/shared/models/request.model';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ChatService {
-
   private readonly API_URL = GLOBAL.API_URL;
-  private headers:HttpHeaders = new HttpHeaders().set('Conten-Type', 'application/json');
+  private headers: HttpHeaders = new HttpHeaders().set(
+    'Content-Type',
+    'application/json'
+  );
 
-  constructor(
-    private http: HttpClient
-  ) { }
+  constructor(private http: HttpClient) {}
 
-  public createChat(request:Request):Observable<any>{
-    return this.http.post(this.API_URL+'chat', request, {headers: this.headers});
+  public createChat(request: Request): Observable<any> {
+    return this.http.post(this.API_URL + 'chat', request, {
+      headers: this.headers,
+    });
   }
 
   /**
    * Update chat user list depending an action, only can be add or remove some user
-   * @param chatId 
-   * @param userId 
-   * @param action 
+   * @param chatId
+   * @param userId
+   * @param action
    * @returns Updated chat
    */
-  public addUser(chatId:string, userId:string, action:string):Observable<any>{
-    return this.http.put
-    (this.API_URL+'chat', { userId, action }, { headers: this.headers, params: { chatId: chatId }});
+  public addUser(
+    chatId: string,
+    userId: string,
+    action: string
+  ): Observable<any> {
+    return this.http.put(
+      this.API_URL + 'chat',
+      { userId, action },
+      { headers: this.headers, params: { chatId: chatId } }
+    );
   }
 }
