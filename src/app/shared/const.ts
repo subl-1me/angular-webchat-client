f7407cc9b4fc628c82429c4aa5462625bdc7f180
subl-1me <julio.ugalde404@gmail.com>
2024-12-08 09:11:26 -0700

    Quick API URI fix

--- src/app/shared/const.ts
+++ src/app/shared/const.ts
@@ -1,17 +1,17 @@
 import { environment } from 'src/environments/environment';
 
 export const GLOBAL = {
-  API_URL: 'http://localhost:3000/api/',
-  socketConfig: {
-    url: 'http://localhost:3000/',
-  },
+  // API_URL: 'http://localhost:3000/api/',
+  // socketConfig: {
+  //   url: 'http://localhost:3000/',
+  // },
   CHAT_SCROLL_DOWN_EVENT_CASES: {
     SEND_MESSAGE_EVENT: 'SEND_MESSAGE',
     OPEN_CHAT_EVENT: 'OPEN_CHAT',
     SEE_UNREAD_MESSAGES_EVENT: 'SEE_UNREAD_MESSAGES_EVENT',
   },
-  // API_URL: environment.API_URL+'/api/',
-  // socketConfig: {
-  //     url: environment.API_URL
-  // }
+  API_URL: environment.API_URL + '/api/',
+  socketConfig: {
+    url: environment.API_URL,
+  },
 };
